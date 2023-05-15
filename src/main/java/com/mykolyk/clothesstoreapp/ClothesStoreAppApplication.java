21863ce921c0414aa0ac3c8baaf793307c547bb7
Myroslav Mykolyk <MyroslavMykolyk@gmail.com>
2023-05-15 22:05:52 +0300

    Initial commit

---
+++ src/main/java/com/mykolyk/clothesstoreapp/ClothesStoreAppApplication.java
@@ -0,0 +1,11 @@
+package com.mykolyk.clothesstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesStoreAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesStoreAppApplication.class, args);
+    }
+}
